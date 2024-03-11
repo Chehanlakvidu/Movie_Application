fbf4d11d7915113dcf7c0c903bc69a53155a65b8
Chehan Lakvidu <lakviduchehan9@gmail.com>
2024-03-11 10:43:04 +0530

    making class to hold movies information

---
+++ lib/movie/movie.dart
@@ -0,0 +1,20 @@
+class Movie{
+  
+  String title;
+  String backDropPath; 
+  String originalTitle;
+  String overview;
+  String posterPath;
+  String releaseDate;
+  double voteAverage;
+
+Movie({
+  required this.title,
+  required this.backDropPath,
+  required this.originalTitle,
+  required this.overview,
+  required this.posterPath,
+  required this.releaseDate,
+  required this.voteAverage,
+});
+}
